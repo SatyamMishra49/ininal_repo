858bb31bf68d4db0fe65b5ce2e41bcd0de27bcd1
SatyamMishra49 <skm28112001@gmail.com>
2025-04-14 23:44:04 +0530

    major fix

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -5,7 +5,7 @@ function Btns() {
     return (
         <button
             type="button"
-            class="text-gray-900 bg-gradient-to-r rounded-full from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium text-sm px-5 py-2.5 text-center me-2 mb-2"
+            class="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
         >
             Continue
         </button>
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -3,15 +3,16 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Btns from '../components/Button';
+import Heading from '../components/Heading';
 import InputFlds from '../components/Inputbox';
-import Logo from '../assets/CashApp.svg';
+import Logo from '../assets/CashApp02.svg';
 
-function SignUp() {
+function SignIn() {
     return (
         <div className="w-screen h-screen flex">
             <div
                 className="w-1/2 p-4 flex items-center justify-center"
-                style={{ backgroundColor: 'oklch(96.7% 0.067 122.328)' }}
+                style={{ backgroundColor: 'rgb(0, 214, 75)' }}
             >
                 <img src={Logo} alt="Logo" className="w-40 h-auto" />
             </div>
@@ -39,14 +40,12 @@ function SignUp() {
                             backgroundColor: '#fff',
                         }}
                     >
-                        <Typography
-                            variant="h5"
-                            fontWeight="bold"
-                            sx={{ mb: 1 }}
-                        >
-                            Log In
+                        <Heading />
+                        <Typography variant="h5" sx={{ mb: 1 }}>
+                            Sign up
                         </Typography>
 
+                        <InputFlds />
                         <InputFlds />
                         <InputFlds />
                         <Btns />
@@ -57,4 +56,4 @@ function SignUp() {
     );
 }
 
-export default SignUp;
+export default SignIn;
