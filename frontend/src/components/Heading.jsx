f1203134225878f00f394f8e64655fa3bbb85db0
SatyamMishra49 <skm28112001@gmail.com>
2025-04-25 18:06:40 +0530

    configured next app

--- frontend/src/components/Heading.jsx
+++
@@ -1,9 +0,0 @@
-import PhonePeLogo from '../assets/PhonePe.svg';
-
-export default function Heading() {
-    return (
-        <div className="p-2 w-[200px]">
-            <img src={PhonePeLogo} alt="phonepe_img"></img>
-        </div>
-    );
-}
